9323e806ad0fa529e5e3ca6939c8c3fe15f85124
David Mašek <masekdave97@gmail.com>
2020-11-03 18:12:12 +0100

    Nedodělaný dom nestihnulo se

--- UHK_PRO2_B_ShoppingCart/src/uhk/fim/gui/MainFrame.java
+++ UHK_PRO2_B_ShoppingCart/src/uhk/fim/gui/MainFrame.java
@@ -1,15 +1,23 @@
 package uhk.fim.gui;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 import uhk.fim.model.ShoppingCart;
 import uhk.fim.model.ShoppingCartItem;
 
 import javax.swing.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.DecimalFormat;
 
 public class MainFrame extends JFrame implements ActionListener {
@@ -99,33 +107,31 @@ public class MainFrame extends JFrame implements ActionListener {
     }
 
     private void createMenuBar() {
-    JMenuBar menuBar = new JMenuBar();
-    JMenu fileMenu = new JMenu("Soubor");
-    fileMenu.add(new AbstractAction("Nový nákupní seznam") {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-        }
-    });
-        fileMenu.add(new AbstractAction("Otevřít") {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Soubor");
+        fileMenu.add(new AbstractAction("Nový nákupní seznam") {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        fileMenu.add(new AbstractAction("Otevřít") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loafFileXmlSax
             }
         });
         fileMenu.add(new AbstractAction("Uložit") {
             @Override
             public void actionPerformed(ActionEvent e) {
-            saveFileCsv();
+                saveFileCsv();
             }
         });
 
-    menuBar.add(fileMenu);
+        menuBar.add(fileMenu);
 
-    JMenu aboutMenu = new JMenu("O programu");
-    menuBar.add(aboutMenu);
-
-
+        JMenu aboutMenu = new JMenu("O programu");
+        menuBar.add(aboutMenu);
 
 
         setJMenuBar(menuBar);
@@ -137,68 +143,130 @@ public class MainFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         // Metoda se volá pro každé tlačítko, musíme tedy rozhodnout, co se má skutečně stát pro konkrétní tlačítka
-        if(actionEvent.getSource() == btnInputAdd) {
-           addProduct();
+        if (actionEvent.getSource() == btnInputAdd) {
+            addProduct();
         }
     }
-    private void addProduct(){
+
+    private void addProduct() {
         boolean nazev = txtInputName.getText().trim().isEmpty();
-        if (nazev){
+        if (nazev) {
             JOptionPane.showMessageDialog(mainFrame, "Nezadal jste název", "Error", JOptionPane.ERROR_MESSAGE);
             return;
         }
         Double doubleZText = 0.0;
 
         try {
-            doubleZText = Double.parseDouble(txtInputPricePerPiece.getText().replace(",","."));
+            doubleZText = Double.parseDouble(txtInputPricePerPiece.getText().replace(",", "."));
 
 
             // System.out.println(doubleZText);
-        }catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(mainFrame, "Nezadal jste správně cena za kus", "Error", JOptionPane.ERROR_MESSAGE);
-                return;
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(mainFrame, "Nezadal jste správně cena za kus", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
         }
 
 
         // Vytvořit novou položku
-        ShoppingCartItem item = new ShoppingCartItem(txtInputName.getText(), doubleZText, (int)spInputPieces.getValue());
+        ShoppingCartItem item = new ShoppingCartItem(txtInputName.getText(), doubleZText, (int) spInputPieces.getValue());
         // Přidat položku do košíku
-       // if (!shoppingCart.updateDuplicity(item)){
-            shoppingCart.addItem(item);
-       // }
+        // if (!shoppingCart.updateDuplicity(item)){
+        shoppingCart.addItem(item);
+        // }
         updateFooter();
         // Refreshnout tabulku
         shoppingCartTableModel.fireTableDataChanged();
         JOptionPane.showMessageDialog(mainFrame, "Super! Přidáno.", "Úspěch", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    private void updateFooter(){
+    private void updateFooter() {
         //double roundedPrize =Math.round((shoppingCart.getTotalPrice() * 100)/100);
 
-       // DecimalFormat f = new DecimalFormat("##.00");
-       // f.format(shoppingCart.getTotalPrice())
-        lblTotalPrice.setText("Celková cena: "+String.format("%.2f" , shoppingCart.getTotalPrice())+" Kč");
+        // DecimalFormat f = new DecimalFormat("##.00");
+        // f.format(shoppingCart.getTotalPrice())
+        lblTotalPrice.setText("Celková cena: " + String.format("%.2f", shoppingCart.getTotalPrice()) + " Kč");
     }
 
-    private void saveFileCsv(){
+    private void saveFileCsv() {
         JFileChooser fc = new JFileChooser();
 
-      if(fc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION){
-        String fileName = fc.getSelectedFile().getAbsolutePath();
-        try (BufferedWriter  bw = new BufferedWriter(new FileWriter(fileName,true));){
+        if (fc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+            String fileName = fc.getSelectedFile().getAbsolutePath();
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, true));) {
 
-            for (ShoppingCartItem item: shoppingCart.getItems()
-                 ) {
-                bw.write(item.getName() + ";" + item.getPricePerPiece() + ";" + item.getPieces());
-                bw.newLine();
+                for (ShoppingCartItem item : shoppingCart.getItems()
+                ) {
+                    bw.write(item.getName() + ";" + item.getPricePerPiece() + ";" + item.getPieces());
+                    bw.newLine();
+                }
+                bw.close();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this, "došlo k chybě", "Error", JOptionPane.ERROR_MESSAGE);
             }
-            bw.close();
-        }catch (IOException e){
-            JOptionPane.showMessageDialog(this,"došlo k chybě", "Error", JOptionPane.ERROR_MESSAGE);
+
+
         }
 
 
-      }
+    }
+
+    private void loafFileXmlSax() {
+        try {
+            CharArrayWriter content = new CharArrayWriter();
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+            parser.parse(new File("src/uhk/fim/save.xml"), new DefaultHandler() {
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    //  System.out.println("start element" + qName);
+                    content.reset();
+                }
+
+
+                @Override
+                public void endElement(String uri, String localName, String qName) throws SAXException {
+                    //  System.out.println("end element" + qName);
+
+                }
+
+
+                @Override
+                public void characters(char[] ch, int start, int length) throws SAXException {
+                    content.write(ch, start, length);
+                }
+            });
+
+            System.out.println(content);
+
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void loafFileXmlDom() {
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = builder.parse(new File("src/uhk/fim/save.xml"));
+            Node root = document.getFirstChild();
+            short nodeType = root.getNodeType();
+            if (root.hasChildNodes()){
+                NodeList list = root.getChildNodes();
+                for (int i = 0; i < list.getLength(); i++) {
+                    Node nextNode = list.item(i);
+                    if (root.hasChildNodes()){
+
+                    }
+                    
+                }
+            }
+
+
+
+
+        } catch (Exception e) {
+
+        }
 
 
     }
